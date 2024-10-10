307d9a06cb23460d1f7d8d42f355ff6e73a5ea48
farelmd <farelmuhdf@gmail.com>
2024-10-10 18:52:15 +0700

    feat: generate reviews

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { BooksModule } from './books/books.module';
 import { GenresModule } from './genres/genres.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, BooksModule, GenresModule],
+  imports: [UsersModule, AuthModule, BooksModule, GenresModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
